1f1b4133141eb34043eb3b80773d43db903d54b9
Dennis Birch <dbirchdev@gmail.com>
2021-04-14 12:17:01 -0700

    Pushed minimum OS version for Mac back to 10.12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SimpleAnalytics",
     platforms: [.iOS(.v10),
-                .macOS(.v10_14),],
+                .macOS(.v10_12),],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
